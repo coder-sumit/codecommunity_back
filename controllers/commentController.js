875522d0d09889b16bfcaf99c19e10e01e4e46d6
sumit <studyzone.sumit@gmail.com>
2023-05-17 18:37:45 +0530

    create api deleteComment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,4 +1,6 @@
 const CCComment = require("../models/cc_comment");
+const CCCommentReply = require("../models/cc_commentReply");
+const Like = require("../models/cc_like");
 const CustomErrorHandler = require("../services/CustomErrorHandler");
 
 
@@ -53,4 +55,45 @@ const editComment = async (req, res, next)=>{
     }
 }
 
-module.exports = {makeComment, editComment};
+const deleteComment = async(req, res, next)=>{
+   try{
+    let comment_id = req.params.id;
+    let user_id = req.user._id;
+
+    if(!comment_id){
+        return next(CustomErrorHandler.invalidInput());
+    }
+
+    // find the comment
+    let comment = await CCComment.findById(comment_id);
+
+    if(comment.user_id != user_id){
+        return next(CustomErrorHandler.unAuthorized());
+    }
+
+    // find all comment replies associated with comment
+    let commentReplies = await CCCommentReply.find({comment_id,});
+
+    // delete all likes associated with comment replies
+    await Promise.all(
+       commentReplies.map(async(comment_reply)=>{
+       await Like.deleteMany({like_target_type: "comment_reply", like_target_id: comment_reply._id});
+       await CCCommentReply.findByIdAndDelete(comment_reply._id);
+    })
+    )
+
+    // delete comment and associated likes
+    await Like.deleteMany({like_target_type: "comment", like_target_id: comment_id});
+    await CCComment.findByIdAndDelete(comment_id);
+
+
+    return res.status(200).json({
+        success: true,
+        message: "comment removed!"
+    });
+   }catch(err){
+     return next(err);
+   }
+}
+
+module.exports = {makeComment, editComment, deleteComment};
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
 
-const {makeComment, editComment} = require("../controllers/commentController"); 
+const {makeComment, editComment, deleteComment} = require("../controllers/commentController"); 
 
 
 router.post("/makeComment",auth, makeComment);
 router.put("/editComment", auth, editComment);
+router.delete("/deleteComment/:id", auth, deleteComment);
 
 
 module.exports = router;
